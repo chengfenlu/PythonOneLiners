fa586e3447ab0a80f034c961df415602a34ae841
chengfenlu <chengfen.lu@gmail.com>
2025-05-27 08:02:14 +0000

    absolute path added

--- book/python_tricks/one_liner_03.py
+++ book/python_tricks/one_liner_03.py
@@ -1,4 +1,4 @@
 # Reading a File
 
-print([line.strip() for line in open("one_liner_03.py")])
+print([line.strip() for line in open("book/python_tricks/one_liner_03.py")])
 # Output: <This file content>
