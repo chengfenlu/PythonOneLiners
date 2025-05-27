94511d01bd1434a4404477a8f77c7fccf1956bdf
chengfenlu <chengfen.lu@gmail.com>
2025-05-27 08:55:30 +0000

    add print

--- book/python_tricks/one_liner_08.py
+++ book/python_tricks/one_liner_08.py
@@ -8,7 +8,7 @@ cardiac_cycle = [62, 60, 62, 64, 68, 77, 80, 76, 71, 66, 61, 60, 62]
 
 ## One-Liner
 expected_cycles = cardiac_cycle[1:-2] * 10
-
+print(expected_cycles)
 ## Result
 plt.plot(expected_cycles)
 plt.show()
